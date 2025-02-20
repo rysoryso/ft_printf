bd861bf0b8c155667296990ada231c26e13694bd
rnomoto <r.nomoto12345@gmail.com>
2025-02-20 18:39:41 +0900

    recoding ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 16:41:45 by rnomoto           #+#    #+#             */
+/*   Updated: 2025/02/20 18:33:17 by rnomoto          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+int	convert(char sign, va_list *ap, int len);
+int	case_c(va_list *ap, int len);
+int	case_s(va_list *ap, int len);
+
+#endif
