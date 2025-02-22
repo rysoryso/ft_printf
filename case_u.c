906681d9cc9384a748c270c59b1821d8202480d3
rnomoto <r.nomoto12345@gmail.com>
2025-02-22 15:27:19 +0900

    0 is 48 in ascii

---
+++ case_u.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   case_u.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 14:14:55 by rnomoto           #+#    #+#             */
+/*   Updated: 2025/02/22 15:25:46 by rnomoto          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int putnbr_unsign(unsigned int nbr, int len)
+{
+    if (nbr > 9)
+        len = putnbr_unsign(nbr / 10, len);
+    ft_putchar_fd(nbr % 10 + 48, 1);
+    len ++;
+
+    return len;
+}
+
+int case_u(va_list *ap, int len)
+{
+    unsigned int n;
+
+    n = va_arg(*ap, unsigned int);
+    return putnbr_unsign(n, len);
+}
