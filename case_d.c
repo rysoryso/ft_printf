7eea5373849d5870d644b79abc10500f4828a59d
rnomoto <r.nomoto12345@gmail.com>
2025-02-22 17:26:38 +0900

    putnbr_base deleted

--- case_d.c
+++ case_d.c
@@ -6,18 +6,35 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 11:01:07 by rnomoto           #+#    #+#             */
-/*   Updated: 2025/02/22 13:55:02 by rnomoto          ###   ########.fr       */
+/*   Updated: 2025/02/22 17:24:07 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int putnbr_dec(int d, int len)
+{
+    long d_long;
+
+    d_long = d;
+    if (d_long < 0)
+    {
+        ft_putchar_fd('-', 1);
+        len ++;
+        d_long *= -1;
+    }
+    if (d_long >= 10 )
+        len = putnbr_dec(d_long / 10, len);
+    ft_putchar_fd(d_long % 10 + 48, 1);
+    len ++;
+
+    return (len);
+}
 
 int case_d(va_list *ap, int len)
 {
     int d;
 
     d = va_arg(*ap, int);
-    len = ft_putnbr_base(d, "0123456789", len);
-
-    return len;
+    return putnbr_dec(d, len);
 }
